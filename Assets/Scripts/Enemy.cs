b42b5bcc7623c754595963c0ff1747ae638736ed
cstrong2 <107379920+cstrong2@users.noreply.github.com>
2022-06-14 22:08:54 +1000

    First Commit - project files
    Assets and project settings

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 20f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+
+    void Die()
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
